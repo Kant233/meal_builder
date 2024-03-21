9d5dfe045774c1d5c2f2fa8345ec0cf51ab4904a
Kyler Anthony <156941935+Kant233@users.noreply.github.com>
2024-03-21 17:00:14 -0500

    Create Test_meal_builder.rb

---
+++ Test_meal_builder.rb
@@ -0,0 +1,26 @@
+require "minitest/autorun"
+require "./recipe-builder"
+
+class TestRecipeBuilder < Minitest::Test
+  def test_protein
+    @recipebuilder = RecipeBuilder.new
+    @protein = "chicken"
+    assert_equal("chicken", @protein)
+  end
+  def test_carbohydrate
+    @recipebuilder = RecipeBuilder.new
+    @carbohydrate = "rice"
+    assert_equal("rice",@carbohydrate)
+  end
+  def test_vegetable 
+    @recipebuilder = RecipeBuilder.new
+    @vegetable = "leeks"
+    assert_equal("leeks", @vegetable)
+  end
+
+end
+
+
+
+ 
+
